d4ab6a849d212451310b83b10e5cf74fcf1909ec
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-03-05 18:22:24 +0100

    added globalStyle

---
+++ src/globalStyle.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+html {
+    box-sizing: border-box;
+};
+
+*,
+::after,
+::before {
+    box-sizing: inherit;
+};
+
+body{
+    font-family: 'Poppins', sans-serif;
+    background-color: ${({ theme }) => theme.color.mainBackground};
+    transition: 0.3s;
+};
+`;
