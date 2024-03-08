64f2b252de93be4bcf4473d5c778ad29d0177d9b
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-03-08 21:57:57 +0100

    added ThemeProvider and GlobalStyle to index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
 import store from "./store";
+import { theme } from "./theme";
+import { GlobalStyle } from './globalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
