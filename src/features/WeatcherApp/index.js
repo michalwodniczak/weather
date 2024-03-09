2da80d77287b6793d7528ee039d487c73d79c1fa
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-03-09 22:26:48 +0100

    added Search and Section Component to WeatcherApp

--- src/features/WeatcherApp/index.js
+++ src/features/WeatcherApp/index.js
@@ -1,9 +1,14 @@
-import { Container } from "../../Container"
-import { Header, } from "../../Header"
+import { Container } from "../../Container";
+import { Header, } from "../../Header";
+import { Section } from "../../common/Section";
+import { Search } from "../../Search";
+
 export const WeatcherApp = () => {
     return (
         <Container>
             <Header />
+            <Search />
+            <Section />
         </Container>
     )
 }
