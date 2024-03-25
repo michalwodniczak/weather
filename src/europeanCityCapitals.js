c4336efb1c3a8032aafd5d982b5b7eb56a47d7a5
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-03-25 18:45:38 +0100

    European country capitals added

---
+++ src/europeanCityCapitals.js
@@ -0,0 +1,16 @@
+export const europeanCityCapitals = [
+    "Warsaw",
+    "Vienna",
+    "Brussels",
+    "Prague",
+    "Copenhagen",
+    "Helsinki",
+    "Paris",
+    "Berlin",
+    "Athens",
+    "Budapest",
+    "Rome",
+    "Amsterdam",
+    "Madrid",
+    "London"
+];
