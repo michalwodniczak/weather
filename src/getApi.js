aceaa940a3e2c1e72fb7d76fee267432b6104626
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-03-28 19:52:32 +0100

    deleted console.log and changed construction

--- src/getApi.js
+++ src/getApi.js
@@ -1,15 +1,21 @@
 import axios from "axios";
+import { europeanCityCapitals } from "./europeanCityCapitals";
 
 const URL = "https://api.openweathermap.org/data/2.5/weather?"
 const apiKey = "fdc13a97817ad7d39b8a876be7b0a7c7";
 
 export const getApi = async () => {
     try {
-        const response = await axios.get(`${URL}units=metric&q=gdansk&APPID=${apiKey}`);
-        // console.log(response.data)
-        // console.log(response.data.name)
-        // console.log(response.data.main)
-        return response.data
+        const randomCities = europeanCityCapitals.sort(() => 0.5 - Math.random()).slice(0, 3);
+        const citiesData = await Promise.all(randomCities.map(async (city) => {
+            const apiUrl = `${URL}q=${city}&appid=${apiKey}&units=metric`
+            const response = await axios.get(apiUrl);
+            return {
+                city: city,
+                weather: response.data,
+            };
+        }));
+        return citiesData;
     }
     catch {
         throw new Error("Failed to search");
