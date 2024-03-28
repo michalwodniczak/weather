956ad3a35dd85d5e19368628cfd4c7118c80b6e7
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-03-28 19:49:59 +0100

    added Item Component

---
+++ src/Item/index.js
@@ -0,0 +1,23 @@
+import { Header, StyledItem, Paragraph, Title, Icon } from "./styled";
+
+export const Item = ({ title, temp, wind, tempFeel, pressure, icon, description }) => {
+    const renderIcon = () => {
+        return (
+            <img src={`http://openweathermap.org/img/w/${icon}.png`} alt="Weather icon" />
+        );
+    };
+
+    return (
+        <StyledItem>
+            <Header>
+                <Title>{title}</Title>
+                <Icon>{renderIcon()}</Icon>
+            </Header>
+            <Paragraph>Temperature: <strong>{temp}</strong></Paragraph>
+            <Paragraph>Feels like: <strong>{tempFeel}</strong></Paragraph>
+            <Paragraph>Wind: <strong>{wind} km/h</strong></Paragraph>
+            <Paragraph>Pressure: <strong>{pressure} hPa</strong></Paragraph>
+            <Paragraph>Description: <strong>{description}</strong></Paragraph>
+        </StyledItem>
+    );
+}
---
+++ src/Item/styled.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const StyledItem = styled.div`
+    width: 300px;
+    max-width: 400px;
+    margin: 10px;
+    border: 1px solid black;
+    border-radius: 5px;
+    padding: 20px;
+`
+export const Header = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+export const Title = styled.h3`
+    font-size: 24px;
+`
+
+export const Paragraph = styled.p`
+    font-size: 16px;
+`
+export const Icon = styled.span`
+    position: relative;
+    top: 5px;
+`
