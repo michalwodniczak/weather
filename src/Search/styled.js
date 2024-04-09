ddec4b3909fbde65a0bbd587f16adcd00a5d6065
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-04-09 17:50:29 +0200

    added media query and refactored code

--- src/Search/styled.js
+++ src/Search/styled.js
@@ -7,19 +7,32 @@ export const Wrapper = styled.div`
 `
 export const Form = styled.form`
     padding: 20px;
+    display: flex;
+    flex-direction: column;
 `
 export const Input = styled.input`
+    margin: 10px auto;
     width: 100%;
+    max-width: 600px;
     text-align: center;
     padding: 10px;
     border-radius: 5px;
     border: 1px solid black;
-    margin: 10px 0;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px){
+       width: calc(100vw - 20px);
+    }
 `
 export const Button = styled.button`
+    margin: 0 auto;
     width: 100%;
+    max-width: 600px;
     border: none;
-    padding:10px;
+    padding: 10px;
     border-radius: 5px;
-    cursor:pointer;
+    cursor: pointer;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px){
+        width: calc(100vw - 20px);
+    }
 `
