4f356bcbc5217f90d2a5ea0d8cf453b379dbe1aa
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-05-09 17:54:15 +0200

    Implement search functionality with API data fetching

---
+++ src/getSearch.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const URL = "https://api.openweathermap.org/data/2.5/weather?"
+const apiKey = "fdc13a97817ad7d39b8a876be7b0a7c7";
+
+export const getSearch = async (query) => {
+    console.log(`query z getSearch ${query}`);
+    try {
+        const fetchURL = `${URL}q=${query}&appid=${apiKey}`;
+        const response = await axios.get(fetchURL);
+        return response.data
+    }
+    catch {
+        throw new Error("Failed to search");
+    }
+}
+
