cb92282adff13283e8757e32c4a0d2ced28254fd
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-06-27 18:01:07 +0200

    added symbol in  <Paragraph>

--- src/Item/index.js
+++ src/Item/index.js
@@ -1,6 +1,6 @@
 import { Header, StyledItem, Paragraph, Title, Icon } from "./styled";
 
-export const Item = ({ title, temp, wind, tempFeel, pressure, icon, description }) => {
+export const Item = ({ title, temp, wind, tempFeel, pressure, icon, description}) => {
     const renderIcon = () => {
         return (
             <img src={`http://openweathermap.org/img/w/${icon}.png`} alt="Weather icon" />
@@ -13,7 +13,7 @@ export const Item = ({ title, temp, wind, tempFeel, pressure, icon, description 
                 <Title>{title}</Title>
                 <Icon>{renderIcon()}</Icon>
             </Header>
-            <Paragraph>Temperature: <strong>{temp}</strong></Paragraph>
+            <Paragraph>Temperature: <strong>{temp} °C</strong></Paragraph>
             <Paragraph>Feels like: <strong>{tempFeel}</strong></Paragraph>
             <Paragraph>Wind: <strong>{wind} km/h</strong></Paragraph>
             <Paragraph>Pressure: <strong>{pressure} hPa</strong></Paragraph>
