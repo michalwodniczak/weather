71d96faf8ae7f22928a357ad03af0a4a51428753
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-07-17 17:51:10 +0200

    added red color and formatted code

--- src/theme.js
+++ src/theme.js
@@ -3,11 +3,11 @@ export const theme = {
         black: "#000000",
         white: "#FFFFFF",
         funBlue: "#1B4A98",
+        red: "#CF0C13",
 
     },
     breakpoints:{
         mobile: "420",
         tablet: "767",
-        
     }
-}
+};
