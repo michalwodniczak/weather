bbb361647d4224e8dce92db555492c7b7b84fe6e
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-07-17 17:52:07 +0200

    deleted console.log

--- src/getSearch.js
+++ src/getSearch.js
@@ -4,7 +4,6 @@ const URL = "https://api.openweathermap.org/data/2.5/weather?"
 const apiKey = "fdc13a97817ad7d39b8a876be7b0a7c7";
 
 export const getSearch = async (query) => {
-    console.log(`query z getSearch ${query}`);
     try {
         const fetchURL = `${URL}q=${query}&appid=${apiKey}&units=metric`;
         const response = await axios.get(fetchURL);
