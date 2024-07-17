95c93e8c450fd0ae6c94effe990b815045445436
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-07-17 17:47:20 +0200

    addded logic to render Component

--- src/features/WeatherApp/WeatherPage/index.js
+++ src/features/WeatherApp/WeatherPage/index.js
@@ -1,35 +1,44 @@
 import { useDispatch, useSelector } from "react-redux";
-import { fetchData, selectData} from "./weatherSlice";
+import { useEffect } from "react";
+import { fetchData, selectData, setStatus } from "./weatherSlice";
 import { Wrapper, Title } from "./styled";
 import { Item } from "../../../Item";
-import { useEffect } from "react";
-
+import { Loading } from "../Loading";
+import { ErrorPage } from "../ErrorPage";
 
 export const WeatherPage = () => {
     const dispatch = useDispatch();
+    const status = useSelector(setStatus);
 
     useEffect(() => {
         dispatch(fetchData())
     }, [dispatch])
     const weatherData = useSelector(selectData);
- 
-    return (
-        <>
-            <Title>Weather in Europe</Title>
-            <Wrapper>
-                {weatherData && weatherData.map(city => (
-                    <Item
-                        key={city.weather.name}
-                        title={city.weather.name}
-                        temp={city.weather.main.temp}
-                        tempFeel={city.weather.main.feels_like}
-                        wind={city.weather.wind.speed}
-                        pressure={city.weather.main.pressure}
-                        icon={city.weather.weather[0].icon}
-                        description={city.weather.weather[0].description}
-                    />
-                ))}
-            </Wrapper>
-        </>
-    )
+
+    switch (status) {
+        case "loading":
+            return <Loading />
+        case "error":
+            return <ErrorPage />
+        default:
+            return (
+                <>
+                    <Title>Weather in Europe</Title>
+                    <Wrapper>
+                        {weatherData && weatherData.map(city => (
+                            <Item
+                                key={city.weather.name}
+                                title={city.weather.name}
+                                temp={city.weather.main.temp}
+                                tempFeel={city.weather.main.feels_like}
+                                wind={city.weather.wind.speed}
+                                pressure={city.weather.main.pressure}
+                                icon={city.weather.weather[0].icon}
+                                description={city.weather.weather[0].description}
+                            />
+                        ))}
+                    </Wrapper>
+                </>
+            )
+    };
 };
