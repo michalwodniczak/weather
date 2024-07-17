d188e27917fd2b169cc27e971be7fb6959847ad2
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-07-17 17:46:39 +0200

    forrmated code

--- src/features/WeatherApp/WeatherPage/weatherSaga.js
+++ src/features/WeatherApp/WeatherPage/weatherSaga.js
@@ -1,4 +1,4 @@
-import { call, put, takeLatest} from "redux-saga/effects";
+import { call, put, takeLatest } from "redux-saga/effects";
 import { getApi } from "../../../getApi";
 import { fetchData, setData, setStatus } from "./weatherSlice";
 
