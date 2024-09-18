c37baa940faa03d7cf4fbcd4f0c20531562d81a1
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-09-18 18:44:26 +0200

    changed logic to render ErrorText

--- src/features/WeatherApp/ErrorPage/index.js
+++ src/features/WeatherApp/ErrorPage/index.js
@@ -1,13 +1,25 @@
 import { useSelector } from "react-redux";
 import { Wrapper, ErrorText, Query } from "./styled";
-import { selectInputValue} from "../../../Search/searchSlice";
+import { selectInputValue, selectData, selectStatus } from "../../../Search/searchSlice";
 
 export const ErrorPage = () => {
     const query = useSelector(selectInputValue);
+    const data = useSelector(selectData);
+    const status = useSelector(selectStatus);
 
-    return (
-        <Wrapper>
-            <ErrorText>Sorry <Query>{query}</Query> is not a City, Please correct this name of City and try again</ErrorText>
-        </Wrapper>
-    );
+    if (data === null && status === "error") {
+        return (
+            <Wrapper>
+                <ErrorText>Sorry we dont have data now</ErrorText>
+            </Wrapper>
+        )
+    };
+
+    if (query && status === "error") {
+        return (
+            <Wrapper>
+                <ErrorText>Sorry <Query>{query}</Query> is not a City, Please correct this name of City and try again</ErrorText>
+            </Wrapper>
+        );
+    };
 };
